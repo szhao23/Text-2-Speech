aa41018c2dfc55f1eaf05fd697a6ae813e4a01b8
szhao23 <stevenzhao456@gmai.com>
2023-08-08 01:57:38 -0500

    Finished HTML, CSS, and JS, Text 2 Speech works with multiple languages option

---
+++ code/src/index.js
@@ -0,0 +1,25 @@
+let speech = new SpeechSynthesisUtterance();
+
+let voices = [];
+
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+  // This will provide all the voice languages on the device
+  voices = window.speechSynthesis.getVoices();
+  // Speak on the default selected language on device
+  speech.voice = voices[0];
+
+  voices.forEach(
+    (voice, i) => (voiceSelect.options[i] = new Option(voice.name, i))
+  );
+};
+
+voiceSelect.addEventListener("change", () => {
+  speech.voice = voices[voiceSelect.value];
+});
+
+document.querySelector("button").addEventListener("click", () => {
+  speech.text = document.querySelector("textarea").value;
+  window.speechSynthesis.speak(speech);
+});
